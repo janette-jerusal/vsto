1a687cf552140d220ba12d0fbd8675e0d4a8f41e
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-22 20:28:07 -0400

    Update ChatThisWorkbook.cs

--- ChatThisWorkbook.cs
+++ ChatThisWorkbook.cs
@@ -1,26 +1,29 @@
 using System;
-using System.Data;
-using System.Drawing;
 using System.Windows.Forms;
-using Microsoft.VisualStudio.Tools.Applications.Runtime;
-using Excel = Microsoft.Office.Interop.Excel;
-using Office = Microsoft.Office.Core;
 
 namespace Test2
 {
     public partial class ThisWorkbook
     {
-        public void Compare()
+        private void ThisWorkbook_Startup(object sender, EventArgs e)
         {
-            MessageBox.Show("hi");
+            // You can put workbook startup logic here if needed.
         }
 
-        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
+        private void ThisWorkbook_Shutdown(object sender, EventArgs e)
         {
+            // Cleanup logic, if needed.
         }
 
-        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
+        #region VSTO generated code
+
+        private void InternalStartup()
         {
+            this.Startup += new EventHandler(ThisWorkbook_Startup);
+            this.Shutdown += new EventHandler(ThisWorkbook_Shutdown);
         }
+
+        #endregion
     }
 }
+
