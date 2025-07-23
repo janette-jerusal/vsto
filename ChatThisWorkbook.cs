ca3419c549b74eb8c0277947f6095e9de9249ab7
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-22 20:21:26 -0400

    Create ChatThisWorkbook.cs

---
+++ ChatThisWorkbook.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace Test2
+{
+    public partial class ThisWorkbook
+    {
+        public void Compare()
+        {
+            MessageBox.Show("hi");
+        }
+
+        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
